e7f576aa64c0d85ab567af4be0be58a0bc32354d
Divyansh9125 <divyanshsrivastava9125@gmail.com>
2022-01-02 16:34:48 +0530

    basic setup

--- screen/HomeScreen.tsx
+++ screen/HomeScreen.tsx
@@ -5,7 +5,7 @@ export const HomeScreen = ({route, navigation }) => {
   
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text>Screen</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
